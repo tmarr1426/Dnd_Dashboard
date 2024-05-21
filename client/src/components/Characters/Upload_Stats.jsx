0662449b6e0f605bb7e555f7f691d5353a45ec5b
tmarr1426 <tmarr1426@gmail.com>
2024-05-21 16:06:00 -0500

    Working on simple updates

---
+++ client/src/components/Characters/Upload_Stats.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ImportokWizard from "@importok/react";
+
+const Upload_Stats = () => {
+  const fields = {
+    character_name: {
+      label: "Character Name",
+    },
+    roll: {
+      label: "Roll",
+    },
+  };
+
+  return (
+    <>
+      <div
+        style={{ marginLeft: "15em", display: "flex", flexDirection: "column" }}
+      >
+        <form>
+          <h2>Create a Character</h2>
+          <label>Character Name</label>
+          <input />
+          <label>Character Class</label>
+          <input />
+          <label>Player Name</label>
+          <input />
+          <label>Character Portrait</label>
+          <input />
+          <button>Add Character</button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Upload_stats;
